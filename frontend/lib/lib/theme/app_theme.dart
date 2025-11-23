ef47142f0d583c67c09df51a01854df8942e669d
Yarok14 Technologies <62498947+Yarok14Technologies@users.noreply.github.com>
2025-11-23 23:41:06 +0530

    Delete frontend/lib directory

--- frontend/lib/lib/theme/app_theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppTheme {
-  static ThemeData lightTheme = ThemeData(
-    primarySwatch: Colors.green,
-    scaffoldBackgroundColor: Colors.white,
-    visualDensity: VisualDensity.adaptivePlatformDensity,
-  );
-}
