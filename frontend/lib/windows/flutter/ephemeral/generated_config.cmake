2e94a1d481d6ec0f1994895a99f88915e7bfacc1
Yarok14 Technologies <62498947+Yarok14Technologies@users.noreply.github.com>
2025-11-23 23:34:06 +0530

    Add files via upload

---
+++ frontend/lib/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\bibin\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\bibin\\OneDrive\\Documents\\Biomethane-Optimization-System\\frontend" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\bibin\\flutter"
+  "PROJECT_DIR=C:\\Users\\bibin\\OneDrive\\Documents\\Biomethane-Optimization-System\\frontend"
+  "FLUTTER_ROOT=C:\\Users\\bibin\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\bibin\\OneDrive\\Documents\\Biomethane-Optimization-System\\frontend\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\bibin\\OneDrive\\Documents\\Biomethane-Optimization-System\\frontend"
+  "FLUTTER_TARGET=C:\\Users\\bibin\\OneDrive\\Documents\\Biomethane-Optimization-System\\frontend\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzkuMC0wLjEucHJl,RkxVVFRFUl9DSEFOTkVMPWJldGE=,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049Y2JiNDY5MzhlZQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YjlhZmIwMzI3NQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS4wIChidWlsZCAzLjExLjAtOTMuMS5iZXRhKQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\bibin\\OneDrive\\Documents\\Biomethane-Optimization-System\\frontend\\.dart_tool\\package_config.json"
+)
